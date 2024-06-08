3ff18b49f31e2fbbfea6ff03f7be857fa83a40b1
Petros Kontos <p_kontos@hotmail.com>
2024-06-09 01:34:55 +0300

    added whitespace

--- src/repl.js
+++ src/repl.js
@@ -10,21 +10,25 @@ const rl = readline.createInterface({
 
 export async function repl() {
   rl.question(USER_HANDLE, async function (input) {
+
     if (input.trim().toLowerCase() === EXIT_COMMAND) {
       console.log(NEWLINE.concat(ASSISTANT_HANDLE).concat(FAREWELL).concat(NEWLINE));
       rl.close();
       return;
     }
+
     await insertToDB(USER, input);
     let history = await selectFromDB();
     const stream = await askLM(history);
     let assistantFullResponse = '';
     process.stdout.write(NEWLINE + ASSISTANT_HANDLE);
+    
     for await (const chunk of stream) {
       const assistantPartialResponse = chunk.choices[0]?.delta?.content || '';
       process.stdout.write(assistantPartialResponse);
       assistantFullResponse += assistantPartialResponse;
     }
+    
     process.stdout.write(NEWLINE);
     await insertToDB(ASSISTANT, assistantFullResponse);
     repl(); // recursion
